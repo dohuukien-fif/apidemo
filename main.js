b5c18f66bce700695650eb416fb5b5a19e131abf
huu kien <huukien0099@gmail.com>
2021-10-15 07:47:57 +0700

    update Ath

--- main.js
+++ main.js
@@ -45,7 +45,7 @@ server.use((req, res, next) => {
 });
 
 // Use default router
-const PORT = process.env.PORT || 3003;
+const PORT = process.env.PORT || 3000;
 server.use(router);
 server.listen(PORT, () => {
   console.log('JSON Server is running');
